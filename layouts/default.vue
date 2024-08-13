a4bc423071f03d8897d3a3d5224f38b21f0b3100
Mosstar <developer@mosstar.com.tr>
2024-08-13 15:06:46 +0300

    chore: Add initial project files and configurations

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="cyan"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon />
+
+      <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template #extension>
+        <v-tabs
+          v-model="tab"
+          align-with-title
+        >
+          <v-tabs-slider color="yellow" />
+
+          <v-tab
+            v-for="item in items"
+            :key="item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      />
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: null,
+      items: [
+        'Home', 'Bölümler', 'Randevularınız', 'Geçmiş Hasta Kayıtları', 'Geçmiş Randevular'
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-range-slider
+    hint="Im a hint"
+    max="50"
+    min="-50"
+  />
+</template>
